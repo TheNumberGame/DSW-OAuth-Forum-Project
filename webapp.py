425e2162738f246112a249f6b25810e1873f6585
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-02-26 09:20:46 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -30,7 +30,7 @@ github = oauth.remote_app(
 #Create and set a global variable for the name of you JSON file here.  The file will be created on Heroku, so you don't need to make it in GitHub
 
 os.system("echo '[]' > " + 'data.json')
-fp = open('data.json', 'r+')
+#fp = open('data.json', 'r+')
 #data = json.load(fp)
 
 @app.context_processor
@@ -45,6 +45,7 @@ def home():
 def post():
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post.
+    fp = open('data.json', 'r+')
     
     try:
         data = json.load(fp)
