b8db7c24747eb3b93367c3976610e6556e197fe7
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-13 13:32:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,7 +21,7 @@ url = 'mongodb://{}:{}@{}:{}/{}'.format(
         os.environ["MONGO_PORT"],
         os.environ["MONGO_DBNAME"])
 
-collection = pymongo.MongoClient(url)[os.environ["MONGO_USERNAME"]]['forum'] 
+collection = pymongo.MongoClient(url)['forum'] #[os.environ["MONGO_USERNAME"]]
 
 print(collection)
 
