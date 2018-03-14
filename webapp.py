a0c3609b88f01a346453314b5a2c367a56e3e500
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-14 14:04:19 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ url = 'mongodb://{}:{}@{}:{}/{}'.format(
         os.environ["MONGO_DBNAME"])
 
 client = pymongo.MongoClient(url)
-client.grantRolesToUser(os.environ["MONGO_USERNAME"], { role: "root", db: os.environ["MONGO_DBNAME"]})
+client.grantRolesToUser(os.environ["MONGO_USERNAME"], { 'role': "root", 'db': os.environ["MONGO_DBNAME"]})
 collection = client[os.environ["MONGO_USERNAME"]]['forum']
 
 
