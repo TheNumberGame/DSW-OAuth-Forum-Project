5fbc8c83ea7672ec4b58afe6e704e27ebbd0b814
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-15 12:18:08 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -70,7 +70,6 @@ def posts_to_html(data = None):
      try:
           for i in data.sort([("date", -1)]): 
                option += Markup("<p id=\"talk\">" + i["name"] + ": " + i["message"] + "</p>"+ "<button type=\"button\" onclick= \{\{ delPost("+str(session['user_data']['login'])+"," + str(i["id"]) +") \}\} > Delete Post</button>")
-               #+ "<button type=\"button\" onClick= \{\{ delPost"+(str(session['user_data']['login'])+ "," + str(i["id"]) + ") \}\} >"+ "Delete Post" + "</button>") +  
      except Exception as ex:
           return str(ex)
      return option    
