3a31f4c0e8ae526bc366afe75b75b16645fa25d3
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-19 11:56:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -69,7 +69,7 @@ def posts_to_html(data = None):
      option = ""
      try:
           for i in data.sort([("date", -1)]):
-               option += Markup("<p id=\"talk\">" + i["name"] + ": " + i["message"] + "</p>"+ "<button id= \"b1\" type=\"button\" value= \""+ str(i["_id"]) +"\"> Delete Post</button>")
+               option += Markup("<p>" + i["name"] + ": " + i["message"] + "</p>"+ "<button id= \"b1\" value= \""+ str(i["_id"]) +"\"> Delete Post</button>")
      except:
           return data
      #print(option)
