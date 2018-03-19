12b57bbf6f0f54994b13693f36c4c6f29d143d7d
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-19 11:53:05 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -75,7 +75,7 @@ def posts_to_html(data = None):
      #print(option)
      return option    
 
-@app.route('/b')
+@app.route('/b', methods=['POST'])
 def delPost():
     id = request.args['id']
     print("Called: "+ str(id))
