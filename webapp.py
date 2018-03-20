2cee859d851f538c2d4964d01212db8501fea2b4
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-20 09:29:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -80,7 +80,7 @@ def delPost():
     id = request.form['id']
     print("Called: "+ str(id))
     
-    collection.delete_one({'_id': {"$oid": id}})
+    collection.delete_one({'_id': {"\$oid": id}})
    
     return render_template('home.html', past_posts = posts_to_html(collection.find()))
 
