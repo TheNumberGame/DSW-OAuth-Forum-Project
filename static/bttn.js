12a4bb91ec231ad379aaaa6185370ac66fa24b5f
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-21 10:49:06 -0700

    Update bttn.js

--- static/bttn.js
+++ static/bttn.js
@@ -1,7 +1,5 @@
 $(document).ready( function(){
   $("button").click( function(){
-    $.post("/b",{id: $(this).val()}, function(){
-      window.location.reload(true)
-    });
+    $.post("/b",{id: $(this).val()});
   });
 });
