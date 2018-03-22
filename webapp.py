842faae2e227265b00d5185b362fc6f472756b07
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-22 10:10:23 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -71,7 +71,7 @@ def posts_to_html(data = None):
           for i in data.sort([("date", -1)]):
                option += Markup("<p>" + i["name"] + ": " + i["message"] + "</p>") 
                if i['name'] == session['user_data']['login']:
-                    option += Markup("<br><input type=\"hidden\" name=\"DeletePost\" value= \""+ str(i["_id"]) +"\">")
+                    option += Markup("<br><button type=\"submit\" name=\"DeletePost\" value= \""+ str(i["_id"]) +"\">Delete Post</button>")
      except Exception as ex:
           return str(ex)
    
