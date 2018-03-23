3ff540b93182546afe5bff506720fd36ed399a7f
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-03-23 11:33:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,7 +13,7 @@ from bson import objectid
 
 app = Flask(__name__)
 
-app.debug = True #Change this to False for production
+app.debug = False #Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
